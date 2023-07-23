5fe77e73f04fe17b65ba21b81fea637a2f5b69f7
norizzz <fantomrizold@gmail.com>
2023-07-24 00:24:27 +0600

    show photos on home page

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,30 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-app.get('/', (req, res) => {
-  res.render('index');
+
+
+router.get("/Players", function(req,res){
+  res.render("players.hbs", {title: 'Players'})
 });
-app.use(express.static('public'));
+
+router.get("/Stats", function(req,res){
+  res.render("stats", {title: ''})
+});
+
+router.get("/Games", function(req,res){
+  res.render("games", {})
+});
+
+router.get("/Halloffame", function(req,res){
+  res.render("halloffame", {})
+});
+
+router.get("/Jersey", function(req,res){
+  res.render("jersey", {})
+});
+router.get("/miamiTextIcon", function(req,res){
+  res.render("../public/images/miamilogo_name.jpeg", {})
+});
+
 
 module.exports = router;
